a7a5ef10e21f2d4d3bda330a0df34e069751bc85
Gena <gena@e64.net>
2016-12-30 16:55:35 +0100

    exercice crm

--- js/app.js
+++ js/app.js
@@ -31,4 +31,8 @@ var customers = [
     }
 ]
 
-// Write you're code here. Enjoy !
+// Write you're code here. Enjoy !   
+
+document.getElementById("1").innerHTML = customers[0].id + "<br>"+customers[0].first_name + "<br>"+customers[0].last_name + "<br>"+customers[0].company+ "<br>"+customers[0].role + "<br>"+customers[0].phone + "<br>"+customers[0].email + "<br>"+ customers[0].description
+document.getElementById("2").innerHTML = customers[1].id + "<br>"+customers[1].first_name + "<br>"+customers[1].last_name + "<br>"+customers[1].company+ "<br>"+customers[1].role + "<br>"+customers[1].phone + "<br>"+customers[1].email + "<br>"+ customers[1].description
+document.getElementById("3").innerHTML = customers[2].id + "<br>"+customers[2].first_name + "<br>"+customers[2].last_name + "<br>"+customers[2].company+ "<br>"+customers[2].role + "<br>"+customers[2].phone + "<br>"+customers[2].email + "<br>"+ customers[2].description
